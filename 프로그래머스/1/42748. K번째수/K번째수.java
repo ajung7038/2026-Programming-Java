0602250b69fe40f20c043c63daa440cfda8fc197
아정 <80907516+ajung7038@users.noreply.github.com>
2026-02-28 18:42:55 +0900

    [level 1] Title: K번째수, Time: 1.05 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,34 +2,18 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        // commands 돌면서 i, j, k 확인
-        // array 복사, 자르기, 정렬, k번째 수 리턴
-        int tcSize = commands.length;
-        int[] result = new int[tcSize];
-        int rsltPointer = 0;
-        
-        for (int[] command : commands) {
-            int i = command[0];
-            int j = command[1];
-            int k = command[2];
+        int[] result = new int[commands.length];
+        for (int t=0; t<commands.length; t++) {
+            int i = commands[t][0];
+            int j = commands[t][1];
+            int k = commands[t][2];
             
-            // i~j까지 arr 복사
-            int arrSize = j-i+1;
-            int[] newArr = new int[arrSize];
-            newArr = copyArr(array, newArr, arrSize, i-1, j-1);
-            // 정렬
+            int[] newArr = Arrays.copyOfRange(array, i-1, j);
+            
             Arrays.sort(newArr);
-            // k값 찾기
-            result[rsltPointer++] = newArr[k-1];
+            result[t] = newArr[k-1];
         }
         
         return result;
-    }
-    
-    public static int[] copyArr(int[] arr, int[] newArr, int arrSize, int start, int end) {
-        for (int i=0; i<arrSize; i++) {
-            newArr[i] = arr[start+i];
-        }
-        return newArr;
     }
 }
